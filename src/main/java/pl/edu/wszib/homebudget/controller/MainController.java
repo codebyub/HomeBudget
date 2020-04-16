42b869a293d29a80d315970dda0c83f08d4ab8c9
ubus <57837724+ubbbb@users.noreply.github.com>
2020-04-16 22:18:00 +0200

    App's layout for its core performance. There are only some handwritten static data in html code for better understanding what the purpose of this app is.

--- src/main/java/pl/edu/wszib/homebudget/controller/MainController.java
+++ src/main/java/pl/edu/wszib/homebudget/controller/MainController.java
@@ -1,7 +1,10 @@
 package pl.edu.wszib.homebudget.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.edu.wszib.homebudget.domain.Expense;
 
 @Controller
 public class MainController {
@@ -10,5 +13,55 @@ public class MainController {
     public String entry() {
         return "entry";
     }
+
+    @GetMapping("/expenses")
+    public String latest() {
+        return "expenses";
+    }
+
+    @GetMapping("/expenses/new")
+    public String newExpense(Model model) {
+        model.addAttribute("outgo", new Expense());
+        return "expense";
+    }
+
+    @GetMapping("/expenses/edit/{id}")
+    public String editExpense(@PathVariable Long id, Model model) {
+        /*write code with interface methods when defined*/
+        return "expense";
+    }
+
+    @GetMapping("/incomes")
+    public String income() {
+        return "incomes";
+    }
+
+    @GetMapping("/incomes/new")
+    public String newIncome(Model model) {
+        model.addAttribute("income", new Expense());
+        return "income";
+    }
+
+    @GetMapping("/incomes/edit/{id}")
+    public String editIncome(@PathVariable Long id, Model model) {
+        /*write code with interface methods when defined*/
+        return "income";
+    }
+
+    @GetMapping("/monthly-balance")
+    public String month() {
+        return "monthly-balance";
+    }
+
+    @GetMapping("/annual-balance")
+    public String year() {
+        return "annual-balance";
+    }
+
+    @GetMapping("/stats")
+    public String stats() {
+        return "stats";
+    }
+
 
 }
---
+++ src/main/java/pl/edu/wszib/homebudget/domain/ExpenseCategory.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.homebudget.domain;
+
+public enum ExpenseCategory {
+    KID,
+    BILL,
+    SAVING,
+    HOUSE,
+    CAR,
+    PET,
+    FOOD,
+    CHEMICAL,
+    HEALTH,
+    CLOTH,
+    RECREATION,
+    GIFT,
+    OTHER
+}
